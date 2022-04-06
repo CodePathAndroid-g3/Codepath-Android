71a3f60ebb0d8e6747889300c64fe95ed21e4be3
HR-Chaos <91175075+HR-Chaos@users.noreply.github.com>
2022-04-05 17:16:59 -0700

    Add files via upload

---
+++ Inform/app/src/main/java/com/example/inform/RegisterActivity.java
@@ -0,0 +1,44 @@
+package com.example.codepathtestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public static final String TAG = "RegisterActivity";
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etEmail = findViewById(R.id.etEmail);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+                RegisterUser(email, username, password);
+            }
+        });
+    }
+
+    private void RegisterUser(String email, String username, String password) {
+        //TODO: put in database
+    }
+
+}
