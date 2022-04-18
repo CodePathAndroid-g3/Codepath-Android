c0714e57e34252c21e94d2618aa8c48bf6692b80
David Ng <chickenbighouse@gmail.com>
2022-04-18 09:27:55 -0700

    two activities and xmls are not connected

--- Inform/app/src/main/java/com/example/inform/LoginActivity.java
+++ Inform/app/src/main/java/com/example/inform/LoginActivity.java
@@ -74,5 +74,9 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
 
 }
